294c3661988b5dbcc6ccf06a827147b9917e362c
zane-dot <22258396@life.hkbu.edu.hk>
2025-04-13 16:58:39 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,9 @@ document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
     const inputText = document.getElementById('inputText').value;
     const resultDiv = document.getElementById('result');
-    // 使用正则表达式进行匹配
-    if (/<script>ALERT\([^)]+\)<\/script>/.test(inputText)) {
-        resultDiv.textContent = 'flag_HPFporkonsale';
+    if (inputText === '<script>ALERT(1)</script>') {
+      resultDiv.textContent = 'flag_HPFporkonsale';
     } else {
-        resultDiv.textContent = 'Keep thinking!This question is about web security.';
+      resultDiv.textContent = 'Keep thinking!This question is about web security.';
     }
-});
+  });
